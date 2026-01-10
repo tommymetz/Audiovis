db9abb321c799fecae32a9b2b20f412b9a7a968a
tommymetz <tom@tommetzmedia.com>
2026-01-09 17:36:44 -0700

    chore: playlist.py cleanup

--- processor/playlist.py
+++ processor/playlist.py
@@ -2,10 +2,12 @@
 # playlist ///////////////////////////////////////////////////
 # ////////////////////////////////////////////////////////////
 import os
-location = '/Users/tometz/Documents/Audio/TomWav/_audiovis/public/content/'
+import json
+
+location = '/Users/tometz/Documents/Clients/Audiovis/stems/'
+location_dest = '/Users/tometz/Documents/Clients/Audiovis/public/content/'
 playlistname = 'Glissline - Horizon EP'
 playlist = []
-
 
 #generate array from each folder
 folders = [x[0] for x in os.walk(location)]
@@ -13,17 +15,18 @@ for i in folders:
     if i != location:
         playlist.append(i[len(location):])
 
+# sort alphabetically
+playlist.sort()
+# Let's move "Horizon" to the end
+if "Horizon" in playlist:
+    playlist.remove("Horizon")
+    playlist.append("Horizon")
+
 #write array to json file
-import json
 data = {}
 data['name'] = playlistname
 data['songs'] = playlist
-with open(location + '_playlist.json', 'w') as outfile:
+with open(location_dest + '_playlist.json', 'w') as outfile:
     json.dump(data, outfile)
 
-print 'playlist', data
-
-
-
-
-# end
+print('playlist', data)
